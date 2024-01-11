8c48e49c9bba8345dba718625aaec77dbd233f09
jesezwa <jessevdzwan@gmail.com>
2024-01-11 13:41:08 +0100

    indexpagina nav toegevoegd

--- index.php
+++ index.php
@@ -1,1 +1,77 @@
- hhoghgh\
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Wilma haakt</title>
+</head>
+
+
+<body>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+
+    <div class="navbar-brand">
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+           data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start pl-4">
+            <a class="navbar-item">
+                Products
+            </a>
+
+            <a class="navbar-item">
+                Projects
+            </a>
+
+            <a class="navbar-item">
+                About us
+            </a>
+
+        </div>
+
+        <div>
+            <a class="navbar-item" href="#"><img src="images/wilmaLogo.png" width="100" class="logo"></a>
+        </div>
+
+        <div class="navbar-end pl-4">
+            <a class="navbar-item">
+                Products
+            </a>
+
+            <a class="navbar-item">
+                Projects
+            </a>
+
+            <a class="navbar-item">
+                About us
+            </a>
+
+        </div>
+    </div>
+
+</nav>
+
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
