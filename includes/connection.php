9beb4dbd6bfd920a14e38516ab809558173de383
Gommpyjoel <joelvmourik@gmail.com>
2024-01-15 14:03:15 +0100

    connection

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,8 @@
 <?php
-$host = "sql.hosted.hro.nl";
-$user = "prj_2023_2024_ressys_t23";
-$password = "oecaesie";
-$database = "prj_2023_2024_ressys_t23";
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "wilma";
 
 $db = mysqli_connect($host, $user, $password, $database)
 or die("Error: " . mysqli_connect_error());
-?>
