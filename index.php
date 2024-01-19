19bf4883628d89443d103eac2bd15732ebe35c93
Gommpyjoel <joelvmourik@gmail.com>
2024-01-19 11:43:22 +0100

    connection

--- index.php
+++ index.php
@@ -91,8 +91,9 @@ require_once 'includes/secure.php';
                         <p class="has-text-centered is-size-4 pb-4"> Bestel hier de beste, mooiste en meest goedkope haakwerken
                             van Nederland
                         </p>
-                            <div class="columns is-centered">
-                                <button class="button is-normal reserve-button">Reserveer hier !</button>
+                            <div class="columns is-centered"><a href="reservate1.php">
+                                <button class="button is-normal reserve-button" > Reserveer hier !</button>
+                                </a>
                             </div>
                     </div>
                 </div>
