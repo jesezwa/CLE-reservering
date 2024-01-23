c62f9f1c1f0820b29583a492d3943b3edf7b5f49
Pascal1400 <1039861@hr.nl>
2024-01-23 11:16:27 +0100

    terms

--- reservate1.php
+++ reservate1.php
@@ -7,6 +7,7 @@ ini_set('display_errors', 1);
 require_once "includes/connection.php";
 require_once 'includes/secure.php';
 
+$user_id = $_SESSION['user_id'];
 
 $date = '';
 
@@ -22,7 +23,14 @@ if(isset($_POST['submit'])) {
     }
 
 
+    if (!empty($_POST('date'))) {
+        $to = $user_id['email'];
+        $subject = "Bevestiging afspraak WILMA";
+        $message = "Heel erg bedankt met het maken van een afsrpaak bij WILMA! Ik kijk uit naar onze afspraak op $date. Het adres is Bob de brouwerstraat 25 in Bodegraven.";
+        $headers = "From: wilmahaakt@gmail.com\r\n";
 
+        mail($to, $subject, $message, $headers);
+    }
 
 }
 
